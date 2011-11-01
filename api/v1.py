f039764067c2aa1b563b360ad59ed2e22cf4e4a4
Sundar Subbarayan <sundar@khanacademy.org>
2011-10-31 21:43:53 -0700

    As a follow-up to change 5023, also updated /api/v1/user to return student_list names instead of keys (to be consistent)

--- api/v1.py
+++ api/v1.py
@@ -274,7 +274,12 @@ def user_data_other():
     if user_data:
         user_data_student = get_visible_user_data_from_request()
         if user_data_student:
-            return user_data_student
+            student_data = user_data_student
+	    list_names = []
+	    for sl in student_data.student_lists:
+		list_names.append(StudentList.get_by_key_name(sl).name)
+            student_data.student_lists = list_names
+	    return student_data
 
     return None
 
