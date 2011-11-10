9906d299628891c2a4399ce73c8132e355aae615
James Irwin <james@khanacademy.org>
2011-11-09 19:21:10 -0800

    removed update tracking

--- profiles/class_exercises_over_time_graph.py
+++ profiles/class_exercises_over_time_graph.py
@@ -24,14 +24,10 @@ def class_exercises_over_time_graph_context(user_data, student_list):
     user_exercise_cache_list = models.UserExerciseCache.get(students_data)
     for i, user_data_student in enumerate(students_data):
         student_nickname = user_data_student.nickname
-        updated = False
-        if not dict_student_exercises.has_key(student_nickname):
-            updated = True
-            dict_student_exercises[student_nickname] = { "nickname": student_nickname, "email": user_data_student.email, "exercises": [] }
+        dict_student_exercises[student_nickname] = { "nickname": student_nickname, "email": user_data_student.email, "exercises": [] }
             
         for exercise, user_exercise in user_exercise_cache_list[i].dicts.iteritems():
             if user_exercise["proficient_date"]:
-                updated = True
                 joined = min(user_data_student.joined, user_exercise["proficient_date"])
                 days_until_proficient = (user_exercise["proficient_date"] - joined).days
                 proficient_date = user_exercise["proficient_date"].strftime('%m/%d/%Y')
@@ -45,6 +41,5 @@ def class_exercises_over_time_graph_context(user_data, student_list):
             "dict_student_exercises": dict_student_exercises,
             "user_data_students": students_data,
             "c_points": reduce(lambda a, b: a + b, map(lambda s: s.points, students_data), 0)
-            }    
-        
-   
+            }
+
