a2ecbda791488866eaa504119e56ef02d447a30d
Desmond Brand <dmnd@desmondbrand.com>
2011-11-28 22:43:43 -0800

    Dedupe method names

--- api/v1.py
+++ api/v1.py
@@ -161,13 +161,13 @@ def playlists_library_list_fresh():
 @route("/api/v1/exercises", methods=["GET"])
 @jsonp
 @jsonify
-def exercises():
+def get_exercises():
     return models.Exercise.get_all_use_cache()
 
 @route("/api/v1/exercises/<exercise_name>", methods=["GET"])
 @jsonp
 @jsonify
-def exercises(exercise_name):
+def get_exercise(exercise_name):
     return models.Exercise.get_by_name(exercise_name)
 
 @route("/api/v1/exercises/<exercise_name>/followup_exercises", methods=["GET"])
