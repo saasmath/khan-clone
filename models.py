0940e2f85eba08842f4e08b960612054f6cc02f2
Ben Kamens <ben@khanacademy.org>
2011-12-13 15:32:22 -0800

    Remove placeholder video test -- it's concluded

--- models.py
+++ models.py
@@ -1456,8 +1456,7 @@ class VideoLog(db.Model):
             user_data.uservideocss_version += 1
             UserVideoCss.set_completed(user_data, user_video.video, user_data.uservideocss_version)
 
-            bingo(['struggling_videos_finished',
-                   'homepage_video_videos_finished'])
+            bingo(['struggling_videos_finished'])
 
         goals_updated = GoalList.update_goals(user_data,
             lambda goal: goal.just_watched_video(user_data, user_video, just_finished_video))
