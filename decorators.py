1c18dbcf3a8f95b9b41e71f2b2bff69115ec9d59
David Hu <david@khanacademy.org>
2011-12-18 02:17:33 -0800

    random bits of code clean up for models.py

--- decorators.py
+++ decorators.py
@@ -1,6 +1,7 @@
 from functools import wraps
 
 def clamp(min_val, max_val):
+    """Ensures wrapped fn's return value is between given min and max bounds"""
     def decorator(func):
         @wraps(func)
         def wrapped(*arg, **kwargs):
