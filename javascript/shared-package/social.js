7f96d5ca7cb9783addaeba922127b8b8c5586546
Ben Komalo <benkomalo@khanacademy.org>
2011-12-27 17:26:54 -0500

    Manually fix issues fixjsstyle is not good enough for yet

--- javascript/shared-package/social.js
+++ javascript/shared-package/social.js
@@ -4,16 +4,16 @@ var Social = {
     init: function(jelContainer) {
         /*--We're using a custom Twitter button, this code enables a popup--*/
         $(".twitterShare", jelContainer).click(function(event) {
-                    var width = 550,
-            height = 370,
-            left = ($(window).width() - width) / 2,
-            top = ($(window).height() - height) / 2,
-            url = this.href,
-            opts = "status=1" +
-            ",width=" + width +
-            ",height=" + height +
-            ",top=" + top +
-            ",left=" + left;
+            var width = 550,
+                height = 370,
+                left = ($(window).width() - width) / 2,
+                top = ($(window).height() - height) / 2,
+                url = this.href,
+                opts = "status=1" +
+                    ",width=" + width +
+                    ",height=" + height +
+                    ",top=" + top +
+                    ",left=" + left;
             window.open(url, "twitter", opts);
             return false;
         });
@@ -29,47 +29,43 @@ var Social = {
 
     facebookBadge: function(desc, icon, ext, activity) {
 
-        FB.ui(
-                    {
-                            method: "feed",
-                            name: "I just earned the " + desc + " badge" + (activity ? " in " + activity : "") + " at Khan Academy!",
-                            link: "http://www.khanacademy.org",
-                            picture: (icon.substring(0, 7) === "http://" ? icon : "http://www.khanacademy.org/" + icon),
-                            caption: "www.khanacademy.org",
-                            description: "You can earn this too if you " + ext
-
-                    });
+        FB.ui({
+            method: "feed",
+            name: "I just earned the " + desc + " badge" + (activity ? " in " + activity : "") + " at Khan Academy!",
+            link: "http://www.khanacademy.org",
+            picture: (icon.substring(0, 7) === "http://" ? icon : "http://www.khanacademy.org/" + icon),
+            caption: "www.khanacademy.org",
+            description: "You can earn this too if you " + ext
+        });
         return false;
 
     },
     facebookVideo: function(name, desc, url) {
 
-        FB.ui(
-                    {
-                            method: "feed",
-                            name: name,
-                            link: "http://www.khanacademy.org/" + url,
-                            picture: "http://www.khanacademy.org/images/handtreehorizontal_facebook.png",
-                            caption: "www.khanacademy.org",
-                            description: desc,
-                            message: "I just learned about " + name + " on Khan Academy"
-                    });
+        FB.ui({
+            method: "feed",
+            name: name,
+            link: "http://www.khanacademy.org/" + url,
+            picture: "http://www.khanacademy.org/images/handtreehorizontal_facebook.png",
+            caption: "www.khanacademy.org",
+            description: desc,
+            message: "I just learned about " + name + " on Khan Academy"
+        });
         return false;
 
     },
 
     facebookExercise: function(amount, plural, prof, exer) {
 
-        FB.ui(
-                    {
-                            method: "feed",
-                            name: amount + " question" + plural + " answered!",
-                            link: "http://www.khanacademy.org/exercisedashboard",
-                            picture: "http://www.khanacademy.org/images/proficient-badge-complete.png",
-                            caption: "www.khanacademy.org",
-                            description: "I just answered " + amount + " question" + plural + " " + prof + " " + exer + " on www.khanacademy.org" ,
-                            message: "I\'ve been practicing " + exer + " on http://www.khanacademy.org"
-                    });
+        FB.ui({
+            method: "feed",
+            name: amount + " question" + plural + " answered!",
+            link: "http://www.khanacademy.org/exercisedashboard",
+            picture: "http://www.khanacademy.org/images/proficient-badge-complete.png",
+            caption: "www.khanacademy.org",
+            description: "I just answered " + amount + " question" + plural + " " + prof + " " + exer + " on www.khanacademy.org" ,
+            message: "I\'ve been practicing " + exer + " on http://www.khanacademy.org"
+        });
         return false;
 
     }
