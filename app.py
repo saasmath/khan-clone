133dae6f4f601c1a7ad6ea63219fefbe87f9e8c4
spicyjalapeno@gmail.com <spicyjalapeno@gmail.com>
2012-01-12 19:20:28 -0500

    Don't need new secrets in development any more

--- app.py
+++ app.py
@@ -4,18 +4,7 @@ try:
     import secrets
 except:
     class secrets(object):
-        facebook_app_id = None
-        facebook_app_secret = None
-        google_consumer_key = None
-        google_consumer_secret = None
-        remote_api_secret = None
-        constant_contact_api_key = None
-        constant_contact_username = None
-        constant_contact_password = None
-        flask_secret_key = None
-        dashboard_secret = None
-        khanbugz_passwd = None
-        paypal_token_id = None
+        pass
 
 # A singleton shared across requests
 class App(object):
@@ -25,31 +14,28 @@ class App(object):
     # during the deployment process.  Minor is always 1 on a dev
     # server.
     version = os.environ.get('CURRENT_VERSION_ID')
-
-    # khanacademy.org
-    facebook_app_id = secrets.facebook_app_id
-    facebook_app_secret = secrets.facebook_app_secret
-
-    google_consumer_key = secrets.google_consumer_key
-    google_consumer_secret = secrets.google_consumer_secret
-
-    remote_api_secret = secrets.remote_api_secret
-
-    constant_contact_api_key = secrets.constant_contact_api_key
-    constant_contact_username = secrets.constant_contact_username
-    constant_contact_password = secrets.constant_contact_password
-
-    flask_secret_key = secrets.flask_secret_key
-
-    dashboard_secret = secrets.dashboard_secret
-
-    khanbugz_passwd = secrets.khanbugz_passwd
-
-    paypal_token_id = secrets.paypal_token_id
-
     root = os.path.dirname(__file__)
-
     is_dev_server = os.environ["SERVER_SOFTWARE"].startswith('Development')
 
     accepts_openid = False
     offline_mode = False
+
+for attr in [
+    'facebook_app_id',
+    'facebook_app_secret',
+    'google_consumer_key',
+    'google_consumer_secret',
+    'remote_api_secret',
+    'constant_contact_api_key',
+    'constant_contact_username',
+    'constant_contact_password',
+    'flask_secret_key',
+    'dashboard_secret',
+    'khanbugz_passwd',
+    'paypal_token_id',
+]:
+    # These secrets are optional in development but not in production
+    if App.is_dev_server and not hasattr(secrets, attr):
+        setattr(App, attr, None)
+    else:
+        setattr(App, attr, getattr(secrets, attr))
