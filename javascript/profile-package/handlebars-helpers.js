fa0f9239692e1a4eefffcd1bedb97e2407081d49
Marcia Lee <marcia@khanacademy.org>
2012-01-16 16:23:55 -0800

    Add commafy handlebars helper for energy points

--- javascript/profile-package/handlebars-helpers.js
+++ javascript/profile-package/handlebars-helpers.js
@@ -1,3 +1,8 @@
 Handlebars.registerHelper("encodeURIComponent", function(str) {
     return encodeURIComponent(str);
 });
+
+Handlebars.registerHelper("commafy", function(numPoints) {
+    // From KhanUtil.commafy in math-format.js
+    return numPoints.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+});
