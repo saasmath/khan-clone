5a9b9a43bd597652ba8e71020b7ab78ff1645b08
Ben Kamens <ben@khanacademy.org>
2012-01-19 16:24:53 -0800

    Workin' on prev/next buttons

--- javascript/stories-package/stories.js
+++ javascript/stories-package/stories.js
@@ -7,6 +7,7 @@ Stories.views = {};
 Stories.render = function(story_data) {
 
     var row = null;
+    var lastStory = null;
     var storiesPerRow = 3;
 
     $.each(story_data.content, function(ix, story) {
@@ -16,10 +17,16 @@ Stories.render = function(story_data) {
             $(story_data.target).append(row);
         }
 
+        if (lastStory) {
+            lastStory.next_story = story;
+            story.prev_story = lastStory;
+        }
+
         var view = new Stories.SmallView({ model: story });
         row.append($(view.render(ix).el));
 
         Stories.views[story.name] = view;
+        lastStory = story;
 
     });
 
@@ -45,7 +52,7 @@ Stories.SmallView = Backbone.View.extend({
             .find(".story")
                 .addClass(ix % 2 == 0 ? "rotate-5" : (ix % 3 == 0 ? "rotate-neg-7" : "rotate-neg-7"))
                 .addClass("envelope-" + ((ix % 3) + 1))
-                .click(function() { Stories.router.navigate("/" + model.name, true); });
+                .click(function() { Stories.navigateTo(model); });
 
         return this;
     },
@@ -58,7 +65,8 @@ Stories.SmallView = Backbone.View.extend({
 
         var model = this.model;
         var jelStory = $(this.el).find(".story");
-        
+        var wasVisible = $("#modal-story").is(":visible");
+
         setTimeout(function() {
 
             $(jelStory).addClass("content-teaser-show");
@@ -66,6 +74,8 @@ Stories.SmallView = Backbone.View.extend({
             setTimeout(function() {
 
                 $(jelStory).addClass("content-teaser-hide");
+
+                $("#modal-story").modal("hide");
 
                 var view = new Stories.FullView({ model: model });
 
@@ -79,11 +89,15 @@ Stories.SmallView = Backbone.View.extend({
                         })
                         .bind("hidden", function() {
                             $(this).remove();
-                            $(".content-teaser-hide")
-                                .removeClass("content-teaser-show")
-                                .removeClass("content-teaser-hide");
 
-                            Stories.router.navigate("");
+                            var isVisible = $("#modal-story").is(":visible");
+                            if (!wasVisible && !isVisible) {
+                                $(".content-teaser-hide")
+                                    .removeClass("content-teaser-show")
+                                    .removeClass("content-teaser-hide");
+
+                                Stories.navigateTo(null);
+                            }
                         });
 
             }, 400);
@@ -91,7 +105,7 @@ Stories.SmallView = Backbone.View.extend({
         }
         , 1);
 
-    }
+    },
 
 });
 
@@ -100,12 +114,31 @@ Stories.FullView = Backbone.View.extend({
     template: Templates.get( "stories.story-full" ),
 
     render: function() {
+        var model = this.model;
+
         $(this.el)
-            .html(this.template(this.model));
+            .html(this.template(this.model))
+           .find(".prev-btn")
+                .not(".disabled")
+                    .click(function() { Stories.navigateTo(model.prev_story); })
+                    .end()
+                .end()
+            .find(".next-btn")
+                .not(".disabled")
+                    .click(function() { Stories.navigateTo(model.next_story); });
         return this;
     }
 
 });
+
+Stories.navigateTo = function(model) {
+    if (model) {
+        Stories.router.navigate("/" + model.name, true);
+    }
+    else {
+        Stories.router.navigate("");
+    }
+};
 
 Stories.StoryRouter = Backbone.Router.extend({
 
