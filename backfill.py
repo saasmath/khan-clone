429f45c28a079635decf14174e5692527d04f603
James Irwin <james@khanacademy.org>
2012-02-15 17:59:17 -0800

    removed a call to get userData for backfill

--- backfill.py
+++ backfill.py
@@ -90,12 +90,10 @@ def user_topic_migration(user_playlist):
     if topic is None:
         raise Exception("User Topic Migration could not find topic for %s" % user_playlist.title)
 
-    user_data = models.UserData.get_from_db_key_email(user_playlist.user.email())
-    if user_data:
-        user_topic = models.UserTopic.get_for_topic_and_user_data(topic, user_data, True)
-        user_topic.seconds_watched += user_playlist.seconds_watched - user_topic.seconds_migrated
-        user_topic.seconds_migrated = user_playlist.seconds_watched
-        user_topic.last_watched = user_playlist.last_watched
-        yield op.db.Put(user_topic)
+    user_topic = models.UserTopic.get_for_topic_and_user(topic, user_playlist.user, True)
+    user_topic.seconds_watched += user_playlist.seconds_watched - user_topic.seconds_migrated
+    user_topic.seconds_migrated = user_playlist.seconds_watched
+    user_topic.last_watched = user_playlist.last_watched
+    yield op.db.Put(user_topic)
 
 
