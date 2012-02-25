5a940ea9b51205bd7b37aafc53551772a3a58c50
Desmond Brand <dmnd@desmondbrand.com>
2012-02-24 17:36:56 -0800

    todos

--- javascript/3XOt1fjWKi8-package/data.js
+++ javascript/3XOt1fjWKi8-package/data.js
@@ -1,5 +1,9 @@
 Socrates.Data["3XOt1fjWKi8"] = {
     Events: [
+        new Socrates.Bookmark({
+            time: "0s",
+            title: "Introduction"
+        }),
         new Socrates.Question({
             time: "24s",
             title: "Numerator and Denominator of a Fraction",
@@ -10,7 +14,14 @@ Socrates.Data["3XOt1fjWKi8"] = {
                 my: "left center",
                 adjust: { x: 230, y: 165 }
             }
+        }),
+        new Socrates.Bookmark({
+            time: "47s",
+            title: "What this fraction represents"
         })
+        // todo(dmnd) Bring in questions from related exercise for the end
+        // What fraction of this circle is shaded red? (recognizing fractions 0.5?)
+        // Given 3/6, what is the fraction's denominator? (recognizing fractions)
     ],
     Skips: []
 };
