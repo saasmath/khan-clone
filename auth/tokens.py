de63f88b86e0a5739a2c46f3c5564cf3ee7631ec
Ben Komalo <benkomalo@khanacademy.org>
2012-02-26 17:35:24 -0800

    s/cookie/token/ig - An "auth token" is a more appropriate name for what this utility is dishing out, since it can be used in cookies or elsewhere.

--- auth/tokens.py
+++ auth/tokens.py
@@ -19,43 +19,43 @@ def _from_timestamp(s):
         return None
     return result
 
-def _make_cookie_signature(user_id,
-                           credential_version,
-                           timestamp,
-                           key=None):
-    """ Generates a signature to be embedded inside of a cookie.
+def _make_token_signature(user_id,
+                          credential_version,
+                          timestamp,
+                          key=None):
+    """ Generates a signature to be embedded inside of an auth token.
     This signature serves two goals. The first is to validate the rest of
-    the contents of the cookie, much like a simple hash. The second is to
+    the contents of the token, much like a simple hash. The second is to
     also encode a unique, user-specific string that can be invalidated if the
     user changes her password (the credential_version).
     """
 
     payload = "\n".join([user_id, credential_version, timestamp])
-    secret = key or App.cookie_recipe_key
+    secret = key or App.token_recipe_key
     return hmac.new(secret, payload, hashlib.sha256).digest()
 
-def mint_cookie_for_user(user_data, clock=None):
-    """ Generates a base64 encoded value to be used as an authentication cookie
-    for a user.
+def mint_token_for_user(user_data, clock=None):
+    """ Generates a base64 encoded value to be used as an authentication token
+    for a user, that can be used in things like cookies.
 
-    The cookie will contain the identity of the user and timestamp of creation,
+    The token will contain the identity of the user and timestamp of creation,
     so expiry logic is externalized and controlled at a higher level.
     """
     user_id = user_data.user_id
     timestamp = _to_timestamp((clock or datetime.datetime).utcnow())
     credential_version = user_data.credential_version
-    signature = _make_cookie_signature(user_id, timestamp, credential_version)
+    signature = _make_token_signature(user_id, timestamp, credential_version)
     return base64.b64encode("\n".join([user_id, timestamp, signature]))
 
-def validate_cookie(user_data, cookie, time_to_expiry=None, clock=None):
-    """ Determines whether or not the cookie is a valid authentication token
+def validate_token(user_data, token, time_to_expiry=None, clock=None):
+    """ Determines whether or not the token is a valid authentication token
     for the specified user.
 
     If time_to_expiry is unspecified, a default of 30 days is used.
     """
 
     try:
-        contents = base64.b64decode(cookie)
+        contents = base64.b64decode(token)
     except TypeError:
         # Not proper base64 encoded value.
         return False
@@ -72,7 +72,7 @@ def validate_cookie(user_data, cookie, time_to_expiry=None, clock=None):
         return False
 
     # Contents look good - now make sure it validates against the sig.
-    expected = _make_cookie_signature(user_data.user_id,
+    expected = _make_token_signature(user_data.user_id,
                                       timestamp,
                                       user_data.credential_version)
     return expected == signature
--- auth/tokens_test.py
+++ auth/tokens_test.py
@@ -30,15 +30,15 @@ class TimestampTests(unittest2.TestCase):
         clock.advance(datetime.timedelta(microseconds=1))
         assertDatetimeSerializes()
 
-class CookieTests(BaseTest):
+class TokenTests(BaseTest):
     def setUp(self):
-        super(CookieTests, self).setUp()
-        self.orig_recipe_key = App.cookie_recipe_key
-        App.cookie_recipe_key = 'secret recipe'
+        super(TokenTests, self).setUp()
+        self.orig_recipe_key = App.token_recipe_key
+        App.token_recipe_key = 'secret recipe'
 
     def tearDown(self):
-        App.cookie_recipe_key = self.orig_recipe_key
-        super(CookieTests, self).tearDown()
+        App.token_recipe_key = self.orig_recipe_key
+        super(TokenTests, self).tearDown()
 
     def make_user(self, user_id, credential_version):
         u = models.UserData.insert_for(user_id, user_id)
@@ -46,36 +46,36 @@ class CookieTests(BaseTest):
         u.put()
         return u
 
-    def test_cookie_expires_properly(self):
+    def test_token_expires_properly(self):
         clock = testutil.MockDatetime()
         u = self.make_user("userid1", "credential version 0")
-        cookie = tokens.mint_cookie_for_user(u, clock)
+        token = tokens.mint_token_for_user(u, clock)
 
         time_to_expiry = datetime.timedelta(30)
-        self.assertTrue(tokens.validate_cookie(u, cookie, time_to_expiry, clock))
+        self.assertTrue(tokens.validate_token(u, token, time_to_expiry, clock))
 
         # The day before expiry
         clock.advance_days(29)
-        self.assertTrue(tokens.validate_cookie(u, cookie, time_to_expiry, clock))
+        self.assertTrue(tokens.validate_token(u, token, time_to_expiry, clock))
 
         # Right at expiring point!
         clock.advance_days(1)
-        self.assertTrue(tokens.validate_cookie(u, cookie, time_to_expiry, clock))
+        self.assertTrue(tokens.validate_token(u, token, time_to_expiry, clock))
 
         # Tick - it's now stale.
         clock.advance(datetime.timedelta(seconds=1))
-        self.assertFalse(tokens.validate_cookie(u, cookie, time_to_expiry, clock))
+        self.assertFalse(tokens.validate_token(u, token, time_to_expiry, clock))
 
-    def test_cookie_invalidates_properly(self):
+    def test_token_invalidates_properly(self):
         clock = testutil.MockDatetime()
         u = self.make_user("userid1", "credential version 0")
-        cookie = tokens.mint_cookie_for_user(u, clock)
+        token = tokens.mint_token_for_user(u, clock)
 
         time_to_expiry = datetime.timedelta(30)
-        self.assertTrue(tokens.validate_cookie(u, cookie, time_to_expiry, clock))
+        self.assertTrue(tokens.validate_token(u, token, time_to_expiry, clock))
 
         # Pretend the user changed her password.
         u.credential_version = "credential version 1"
         u.put()
-        self.assertFalse(tokens.validate_cookie(u, cookie, time_to_expiry, clock))
+        self.assertFalse(tokens.validate_token(u, token, time_to_expiry, clock))
 
