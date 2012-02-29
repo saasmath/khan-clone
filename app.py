885c46fee4c3b739daa43c388ca79d62b0260c90
Ben Komalo <benkomalo@khanacademy.org>
2012-02-29 11:22:22 -0800

    Add in clarifying comment regarding token_recipe_key

--- app.py
+++ app.py
@@ -41,6 +41,7 @@ for attr in [
         setattr(App, attr, getattr(secrets, attr))
 
 if App.is_dev_server and App.token_recipe_key is None:
-    # Set to random keymash
+    # If a key is missing to dishout auth tokens on dev, we can't login with
+    # our own auth system. So just set it to a random string.
     App.token_recipe_key = 'lkj9Hg7823afpEOI3nmlkfl3jfnklsfQQ'
 
