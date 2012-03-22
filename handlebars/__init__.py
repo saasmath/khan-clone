eeb4931e7457de5fb6c96d4b334376102421eb73
Tom W. Yedwab <tom@khanacademy.org>
2012-03-21 17:38:00 -0700

    Fixing dynamic PyBars compilation.
    Dynamically load Handlebars templates from source, including dependent partials. (Disabled in production)

--- handlebars/__init__.py
+++ handlebars/__init__.py
@@ -2,6 +2,7 @@ import sys
 import re
 import urllib
 import logging
+from app import App
 
 from pybars import Compiler
 
@@ -29,11 +30,38 @@ handlebars_helpers = {
     "commafy": handlebars_commafy
 }
 
-# Invoke a template and return the output string
-def handlebars_template(package, name, params):
-    partials = dict()
+def handlebars_dynamic_load(package, name):
+    """ Dynamically compile a Handlebars template.
+        Do not do this in production mode! """
 
-    original_file_name = "javascript/%s-package/%s.handlebars" % (package, name)
+    if not App.is_dev_server:
+        return None
+
+    combined_name = "%s_%s" % (package, name)
+    if combined_name in handlebars_partials:
+        handlebars_partials[combined_name]
+
+    logging.info("Dynamically loading %s-package/%s.handlebars." % (package, name))
+    file_name = "clienttemplates/%s-package/%s.handlebars" % (package, name)
+
+    in_file = open(file_name, 'r')
+    source = unicode(in_file.read())
+    source = source.replace("{{else}}", "{{^}}") # Pybars doesn't handle {{else}} for some reason
+
+    matches = re.search('{{>[\s]*([\w\-_]+)[\s]*}}', source)
+    if matches:
+        for partial in matches.groups():
+            (partial_package, partial_name) = partial.split("_")
+            handlebars_dynamic_load(partial_package, partial_name)
+
+    compiler = Compiler()
+    function = compiler.compile(source)
+    handlebars_partials[combined_name] = function
+
+    return function
+
+def handlebars_template(package, name, params):
+    """ Invoke a template and return the output string """
 
     package_name = package.replace("-", "_")
     function_name = name.replace("-", "_")
@@ -53,17 +81,10 @@ def handlebars_template(package, name, params):
     if module_name in sys.modules:
         function = getattr(sys.modules[module_name], function_name)
 
+    # Fallback is to compile the template dynamically.
+    # In production mode, this does nothing.
     if not function:
-        try:
-            in_file = open(original_file_name, 'r')
-            source = unicode(in_file.read())
-            source = source.replace("{{else}}", "{{^}}") # Pybars doesn't handle {{else}} for some reason
-
-            compiler = Compiler()
-            function = compiler.compile(source)
-        except:
-            logging.info("Compile error!")
-            return u""
+        function = handlebars_dynamic_load(package, name)
 
     ret = function(params, helpers=handlebars_helpers, partials=handlebars_partials)
     return u"".join(ret)
