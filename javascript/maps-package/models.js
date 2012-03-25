0fb130af62a825d4c57e4cc067b49e52a6e0116a
Ben Kamens <ben@khanacademy.org>
2012-03-24 19:22:03 -0700

    Pull common knowledge map node model properties out into KnowledgeMapModels.Node -- lots of this diff is indentation change

--- javascript/maps-package/models.js
+++ javascript/maps-package/models.js
@@ -2,74 +2,103 @@
 // TODO: would be nice if this were part of a larger KnowledgeMap context
 // instead of needing the KnowledgeMap naming prefix.
 
-/*
+/**
  * All models that may be represented on the knowledge map
  */
-var KnowledgeMapModels = {
+var KnowledgeMapModels = {};
 
-    Topic: Backbone.Model.extend({
+/**
+ * All models rendered as nodes on the knowledge map extend Node
+ */
+KnowledgeMapModels.Node = Backbone.Model.extend({
 
-        initialize: function(attributes) {
+    /**
+     * Set all required properties for rendering map node
+     */
+    setNodeAttrs: function(name, display_name, x, y, zoomBounds) {
 
-            // Translate topic properties to standard node properties
-            this.set({
-                name: this.get("id"),
-                h_position: this.get("x"),
-                v_position: this.get("y"),
-                display_name: this.get("standalone_title"),
-                lowercaseName: this.get("standalone_title").toLowerCase(),
-                zoomBounds: [KnowledgeMapGlobals.options.minZoom, KnowledgeMapGlobals.options.minZoom],
-            });
+        var iconUrl = KnowledgeMapGlobals.icons.Exercise[this.get("status")] || KnowledgeMapGlobals.icons.Exercise.Normal;
 
-            return Backbone.Model.prototype.initialize.call(this, attributes);
+        this.set({
+            name: name,
+            h_position: x,
+            v_position: y,
+            display_name: display_name,
+            lowercaseName: display_name.toLowerCase(),
+            inAllList: false, // TODO(kamens): remove?
+            zoomBounds: zoomBounds,
+            iconUrl: iconUrl
+        });
 
-        },
+    }
 
-        url: function() {
-            return "/topicexercise/" + this.get("id");
-        },
+});
 
-    }),
+KnowledgeMapModels.Topic = KnowledgeMapModels.Node.extend({
 
-    Exercise: Backbone.Model.extend({
-        initialize: function(attributes) {
+    initialize: function(attributes) {
 
-            // TODO(kamens): remove/replace w/ simple streak bar/icon?
-            if (this.get("status") == "Suggested") {
-                this.set({"isSuggested": true, "badgeIcon": "/images/node-suggested.png?" + KA_VERSION});
-            } else if (this.get("status") == "Review") {
-                this.set({"isSuggested": true, "isReview": true, "badgeIcon": "/images/node-review.png?" + KA_VERSION});
-            } else if (this.get("status") == "Proficient") {
-                this.set({"isSuggested": false, "badgeIcon": "/images/node-complete.png?" + KA_VERSION});
-            } else {
-                this.set({"isSuggested": false, "badgeIcon": "/images/node-not-started.png?" + KA_VERSION});
-            }
+        // Translate topic properties to standard node properties
+        this.setNodeAttrs(
+            this.get("id"),
+            this.get("standalone_title"),
+            this.get("x"),
+            this.get("y"),
+            [KnowledgeMapGlobals.options.minZoom, KnowledgeMapGlobals.options.minZoom]
+        );
 
-            this.set({
-                inAllList: false,
-                lowercaseName: this.get("display_name").toLowerCase(),
-                zoomBounds: [KnowledgeMapGlobals.options.minZoom + 1, KnowledgeMapGlobals.options.maxZoom]
-            });
+        return KnowledgeMapModels.Node.prototype.initialize.call(this, attributes);
 
-            // TODO(kamens): remove/replace w/ simple streak bar/icon?
-            this.set({"streakBar": {
-                "proficient": this.get("progress") >= 1,
-                "suggested": (this.get("status") == "Suggested" || (this.get("progress") < 1 && this.get("progress") > 0)),
-                "progressDisplay": this.get("progress_display"),
-                "maxWidth": 228,
-                "width": Math.min(1.0, this.get("progress")) * 228
-            }});
+    },
 
-            return Backbone.Model.prototype.initialize.call(this, attributes);
-        },
+    url: function() {
+        return "/topicexercise/" + this.get("id");
+    }
 
-        url: function() {
-            return "/exercise/" + this.get("name");
-        },
+});
 
-        adminUrl: function() {
-            return "/editexercise?name=" + this.get("name");
+KnowledgeMapModels.Exercise = KnowledgeMapModels.Node.extend({
+
+    initialize: function(attributes) {
+
+        // TODO(kamens): remove/replace w/ simple streak bar/icon?
+        if (this.get("status") == "Suggested") {
+            this.set({"isSuggested": true, "badgeIcon": "/images/node-suggested.png?" + KA_VERSION});
+        } else if (this.get("status") == "Review") {
+            this.set({"isSuggested": true, "isReview": true, "badgeIcon": "/images/node-review.png?" + KA_VERSION});
+        } else if (this.get("status") == "Proficient") {
+            this.set({"isSuggested": false, "badgeIcon": "/images/node-complete.png?" + KA_VERSION});
+        } else {
+            this.set({"isSuggested": false, "badgeIcon": "/images/node-not-started.png?" + KA_VERSION});
         }
-    })
 
-}
+        // Translate exercise properties to standard node properties
+        this.setNodeAttrs(
+            this.get("name"),
+            this.get("display_name"),
+            this.get("h_position"),
+            this.get("v_position"),
+            [KnowledgeMapGlobals.options.minZoom + 1, KnowledgeMapGlobals.options.maxZoom]
+        );
+
+        // TODO(kamens): remove/replace w/ simple streak bar/icon?
+        this.set({"streakBar": {
+            "proficient": this.get("progress") >= 1,
+            "suggested": (this.get("status") == "Suggested" || (this.get("progress") < 1 && this.get("progress") > 0)),
+            "progressDisplay": this.get("progress_display"),
+            "maxWidth": 228,
+            "width": Math.min(1.0, this.get("progress")) * 228
+        }});
+
+        return KnowledgeMapModels.Node.prototype.initialize.call(this, attributes);
+    },
+
+    url: function() {
+        return "/exercise/" + this.get("name");
+    },
+
+    adminUrl: function() {
+        return "/editexercise?name=" + this.get("name");
+    }
+
+});
