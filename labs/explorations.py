183a7998c42f7b5879730cbb0d1d4d7ecc409e27
Ben Alpert <spicyjalapeno@gmail.com>
2012-03-29 22:24:43 -0400

    Add now-required @open_access decorator

--- labs/explorations.py
+++ labs/explorations.py
@@ -1,5 +1,6 @@
 from api.auth.xsrf import ensure_xsrf_cookie
 from custom_exceptions import MissingExerciseException
+from user_util import open_access
 import request_handler
 
 EXPLORATIONS = [
@@ -9,6 +10,7 @@ EXPLORATIONS = [
 ]
 
 class RequestHandler(request_handler.RequestHandler):
+    @open_access
     @ensure_xsrf_cookie
     def get(self, exploration=None):
         if not exploration:
