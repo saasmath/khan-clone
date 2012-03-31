02f409566cd317ce4bcb2d18dee6f49aadd93636
James Irwin <james@khanacademy.org>
2012-03-31 14:25:57 -0700

    removed inApp memory layer from common core - it might be too large?

--- common_core/models.py
+++ common_core/models.py
@@ -152,7 +152,8 @@ class CommonCoreMap(db.Model):
 
     @staticmethod
     @layer_cache.cache_with_key_fxn(
-        key_fxn=lambda lightweight: "structured_cc:%s" % lightweight)
+        key_fxn=lambda lightweight: "structured_cc:%s" % lightweight,
+        layer=layer_cache.Layers.Memcache)
     def get_all_structured(lightweight=False):
         all_entries = [
                 { 'grade': 'K', 'domains': [] }, { 'grade': '1', 'domains': [] }, { 'grade': '2', 'domains': [] },
