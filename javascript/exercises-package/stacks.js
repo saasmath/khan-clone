3059622fd69fe56d3fb88fb798c41e3f2fc1d320
spicyjalapeno@gmail.com <spicyjalapeno@gmail.com>
2012-03-31 22:06:44 -0400

    Suggest "Take a Break" if you're proficient in everything

--- javascript/exercises-package/stacks.js
+++ javascript/exercises-package/stacks.js
@@ -569,9 +569,10 @@ Exercises.CurrentCardView = Backbone.View.extend({
 
                     return _.extend(
                         {
-                            "practiceMode": Exercises.practiceMode,
-                            "proficient": proficientExercises.length,
-                            "total": topicUserExercises.length,
+                            practiceMode: Exercises.practiceMode,
+                            proficient: proficientExercises.length,
+                            total: topicUserExercises.length,
+                            allProficient: proficientExercises.length == topicUserExercises.length,
                             startedExercises: startedExercises,
                             unstartedExercises: unstartedExercises,
                             proficientExercises: proficientExercises
@@ -601,6 +602,8 @@ Exercises.CurrentCardView = Backbone.View.extend({
                             .click(function() {
                                 window.location.reload();
                             })
+                            .end()
+                        .find(".simple-button.green")
                             .focus();
                 }
             );
