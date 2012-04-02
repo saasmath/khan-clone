d6c0e41ac8df637e7f4d1dd6d558dac73165ee2a
spicyjalapeno@gmail.com <spicyjalapeno@gmail.com>
2012-04-02 18:52:41 -0400

    Remove getHorizontallyRepeatingTileUrl since it didn't do anything

--- javascript/maps-package/knowledgemap.js
+++ javascript/maps-package/knowledgemap.js
@@ -25,48 +25,21 @@ function KnowledgeMapInitGlobals() {
         lngMax: -180,
         nodeSpacing: {lat: 0.392, lng: 0.35},
         options: {
-                    getTileUrl: function(coord, zoom) {
-                        // Sky tiles example from
-                        // http://gmaps-samples-v3.googlecode.com/svn/trunk/planetary-maptypes/planetary-maptypes.html
-                        return KnowledgeMapGlobals.getHorizontallyRepeatingTileUrl(coord, zoom,
-                                function(coord, zoom) {
-                                  return "/images/map-tiles/field_" +
-                                     Math.floor(Math.random() * 4 + 1) + ".jpg";
-                                });
-                    },
-                    tileSize: new google.maps.Size(256, 256),
-                    maxZoom: 9,
-                    minZoom: 6,
-                    isPng: false
+            getTileUrl: function(coord, zoom) {
+                // Sky tiles example from
+                // http://gmaps-samples-v3.googlecode.com/svn/trunk/planetary-maptypes/planetary-maptypes.html
+                return "/images/map-tiles/field_" + Math.floor(Math.random() * 4 + 1) + ".jpg";
+            },
+            tileSize: new google.maps.Size(256, 256),
+            maxZoom: 9,
+            minZoom: 6,
+            isPng: false
         },
 
         xyToLatLng: function(x, y) {
             return new google.maps.LatLng(
                     -1 * (y - 1) * KnowledgeMapGlobals.nodeSpacing.lat, x * KnowledgeMapGlobals.nodeSpacing.lng
             );
-        },
-
-        getHorizontallyRepeatingTileUrl: function(coord, zoom, urlfunc) {
-
-            // From http://gmaps-samples-v3.googlecode.com/svn/trunk/planetary-maptypes/planetary-maptypes.html
-            var y = coord.y;
-            var x = coord.x;
-
-            // tile range in one direction range is dependent on zoom level
-            // 0 = 1 tile, 1 = 2 tiles, 2 = 4 tiles, 3 = 8 tiles, etc
-            var tileRange = 1 << zoom;
-
-            // don't repeat across y-axis (vertically)
-            if (y < 0 || y >= tileRange) {
-                return null;
-            }
-
-            // repeat across x-axis
-            if (x < 0 || x >= tileRange) {
-                x = (x % tileRange + tileRange) % tileRange;
-            }
-
-            return urlfunc({x: x, y: y}, zoom);
         }
     };
 }
