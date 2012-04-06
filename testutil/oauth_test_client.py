1460f11cc1757837db7de0a145d29a69b7bf8f11
Craig Silverstein <csilvers@khanacademy.org>
2012-04-06 16:27:51 -0700

    Get rid of an unused import.  Found by pychecker!

--- testutil/oauth_test_client.py
+++ testutil/oauth_test_client.py
@@ -36,7 +36,6 @@ from google.appengine.ext.remote_api import remote_api_stub
 # This import needs to happen first to avoid problems with circular
 # imports.  Sigh.
 import models
-import api.auth
 from oauth_provider import consts
 from oauth_provider import models_oauth
 from oauth_provider import oauth
