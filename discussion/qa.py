0498c8f21c68e35099e7475c952b930e23673c41
Marcia Lee <marcia@khanacademy.org>
2012-04-10 00:37:59 -0700

    Remove topic traces from q&a code
    Historical fun fact: there was a day when non-js users were able to view all discussion content through the magic of links-that-worked. That was a great day. Considering that the video doesn't play without JS and this behavior has been broken for a while without complaints, this code is going away.

--- discussion/qa.py
+++ discussion/qa.py
@@ -46,7 +46,6 @@ class PageQuestions(request_handler.RequestHandler):
             pass
 
         video_key = self.request.get("video_key")
-        topic_key = self.request.get("topic_key")
         qa_expand_key = self.request_string("qa_expand_key")
         sort = self.request_int("sort", default=-1)
         video = db.get(video_key)
@@ -54,12 +53,7 @@ class PageQuestions(request_handler.RequestHandler):
         user_data = models.UserData.current()
 
         if video:
-            try:
-                topic = db.get(topic_key)
-            except db.BadKeyError:
-                topic = video.first_topic()
-
-            template_values = video_qa_context(user_data, video, topic, page, qa_expand_key, sort)
+            template_values = video_qa_context(user_data, video, page, qa_expand_key, sort)
             html = self.render_jinja2_template_to_string("discussion/video_qa_content.html", template_values)
             self.render_json({"html": html, "page": page, "qa_expand_key": qa_expand_key})
 
@@ -164,7 +158,6 @@ class AddQuestion(request_handler.RequestHandler):
 
         question_text = self.request.get("question_text")
         video_key = self.request.get("video_key")
-        topic_key = self.request.get("topic_key")
         video = db.get(video_key)
         question_key = ""
 
@@ -185,8 +178,8 @@ class AddQuestion(request_handler.RequestHandler):
             question.put()
             question_key = question.key()
 
-        self.redirect("/discussion/pagequestions?video_key=%s&topic_key=%s&qa_expand_key=%s" % 
-                (video_key, topic_key, question_key))
+        self.redirect("/discussion/pagequestions?video_key=%s&qa_expand_key=%s" % 
+                (video_key, question_key))
 
 class EditEntity(request_handler.RequestHandler):
     @disallow_phantoms
@@ -197,7 +190,6 @@ class EditEntity(request_handler.RequestHandler):
             return
 
         key = self.request.get("entity_key")
-        topic_key = self.request.get("topic_key")
         text = self.request.get("question_text") or self.request.get("answer_text")
 
         if key and text:
@@ -214,8 +206,8 @@ class EditEntity(request_handler.RequestHandler):
 
                         page = self.request.get("page")
                         video = feedback.video()
-                        self.redirect("/discussion/pagequestions?video_key=%s&topic_key=%s&page=%s&qa_expand_key=%s" % 
-                                        (video.key(), topic_key, page, feedback.key()))
+                        self.redirect("/discussion/pagequestions?video_key=%s&page=%s&qa_expand_key=%s" % 
+                                        (video.key(), page, feedback.key()))
 
                     elif feedback.is_type(models_discussion.FeedbackType.Answer):
 
@@ -304,7 +296,7 @@ class DeleteEntity(request_handler.RequestHandler):
 
         self.redirect("/discussion/flaggedfeedback")
 
-def video_qa_context(user_data, video, topic=None, page=0, qa_expand_key=None, sort_override=-1):
+def video_qa_context(user_data, video, page=0, qa_expand_key=None, sort_override=-1):
     limit_per_page = 5
 
     if page <= 0:
@@ -360,7 +352,6 @@ def video_qa_context(user_data, video, topic=None, page=0, qa_expand_key=None, s
     return {
             "is_mod": user_util.is_current_user_moderator(),
             "video": video,
-            "topic": topic,
             "questions": questions,
             "count_total": count_total,
             "pages": range(1, pages_total + 1),
