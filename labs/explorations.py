c0716f4ac1c1e910c01bc532f234665a9c770249
David Sissitka <me@dsissitka.com>
2012-04-13 16:02:47 -0400

    Added "Perfect Secrecy" to the labs.

--- labs/explorations.py
+++ labs/explorations.py
@@ -6,7 +6,8 @@ import request_handler
 EXPLORATIONS = [
     # Crypto
     'frequency-fingerprint',
-    'frequency-stability'
+    'frequency-stability',
+    'perfect-secrecy'
 ]
 
 class RequestHandler(request_handler.RequestHandler):
