derekzhang79/khan-website

Several bug fixes in PyBars, and improved handling of partials.
Hackaround for PyBars not recognizing {{else}}.
Support for .length paths for arrays.
Automatic partial registration in Python.
Changed the registered partial names to be in a "package_template" format so we can deduce the original filename.
Adding a bunch of template unit tests (.json files)

More PyBars fixes, and a lot of work on partials.

javascript/profile-package/class-progress-summary.js
@@ -45,7 +45,7 @@ var ProgressSummaryView = function() {
         fInitialized = true;
 
         // Register partials and helpers
-        Handlebars.registerPartial("class-progress-column", Templates.get("profile.class-progress-column"));
+        Handlebars.registerPartial("profile_class-progress-column", Templates.get("profile.class-progress-column"));
 
         Handlebars.registerHelper("toPixelWidth", function(num) {
             return toPixelWidth(num);

saasmath/khan-clone

javascript/studentlists-package/class-progress-summary.js
@@ -45,7 +45,7 @@ var ProgressSummaryView = function() {
         fInitialized = true;
 
         // Register partials and helpers
-        Handlebars.registerPartial("class-progress-column", Templates.get("studentlists.class-progress-column"));
+        Handlebars.registerPartial("studentlists_class-progress-column", Templates.get("studentlists.class-progress-column"));
 
         Handlebars.registerHelper("toPixelWidth", function(num) {
             return toPixelWidth(num);