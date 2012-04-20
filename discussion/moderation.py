a5d2ec7026522d6b2af3cdef45ce258f2e0acaa0
Marcia Lee <marcia@khanacademy.org>
2012-04-20 01:21:16 -0700

    Lint fixes for moderation.py, review 1677

--- discussion/moderation.py
+++ discussion/moderation.py
@@ -1,5 +1,4 @@
 from google.appengine.ext import db
-from google.appengine.api import users
 
 import api.auth.xsrf
 import request_handler
@@ -8,31 +7,36 @@ import discussion_models
 from badges.discussion_badges import ModeratorBadge
 import user_util
 
-class ModPanel(request_handler.RequestHandler):
 
+class ModPanel(request_handler.RequestHandler):
     @user_util.moderator_only
     def get(self):
-        self.render_jinja2_template('discussion/mod/mod.html', { "selected_id": "panel" })
+        template_values = {
+            'selected_id': 'panel',
+        }
+        self.render_jinja2_template('discussion/mod/mod.html',
+                                    template_values)
+
 
 class ModeratorList(request_handler.RequestHandler):
-
-    # Must be an admin to change moderators
     @user_util.admin_only
     @api.auth.xsrf.ensure_xsrf_cookie
     def get(self):
-        mods = user_models.UserData.gql("WHERE moderator = :1", True)
-        self.render_jinja2_template('discussion/mod/moderatorlist.html', {
-            "mods" : mods,
-            "selected_id": "moderatorlist",
-        })
+        mods = user_models.UserData.gql('WHERE moderator = :1', True)
+        template_values = {
+            'mods': mods,
+            'selected_id': 'moderatorlist',
+        }
+        self.render_jinja2_template('discussion/mod/moderatorlist.html',
+                                    template_values)
 
     @user_util.admin_only
     @api.auth.xsrf.ensure_xsrf_cookie
     def post(self):
-        user_data = self.request_user_data("user")
+        user_data = self.request_user_data('user')
 
         if user_data:
-            user_data.moderator = self.request_bool("mod")
+            user_data.moderator = self.request_bool('mod')
 
             if user_data.moderator:
                 if not ModeratorBadge().is_already_owned_by(user_data):
@@ -40,51 +44,60 @@ class ModeratorList(request_handler.RequestHandler):
 
             db.put(user_data)
 
-        self.redirect("/discussion/mod/moderatorlist")
+        self.redirect('/discussion/mod/moderatorlist')
+
 
 class FlaggedFeedback(request_handler.RequestHandler):
-
     @user_util.moderator_only
     def get(self):
-
         # Show all non-deleted feedback flagged for moderator attention
-        feedback_query = discussion_models.Feedback.all().filter("is_flagged = ", True).filter("deleted = ", False)
+        feedback_query = discussion_models.Feedback.all()
+        feedback_query = feedback_query.filter('is_flagged = ', True)
+        feedback_query = feedback_query.filter('deleted = ', False)
 
         feedback_count = feedback_query.count()
 
-        # Grab a bunch of flagged pieces of feedback and point moderators at the 50 w/ lowest votes first.
-        # ...can easily do this w/ an order on the above query and a new index, but avoiding the index for now
-        # since it's only marginally helpful.
+        # Grab a bunch of flagged pieces of feedback and point moderators at
+        # the 50 w/ lowest votes first.
+        # ...can easily do this w/ an order on the above query and a new index,
+        # but avoiding the index for now since it's only marginally helpful.
         feedbacks = feedback_query.fetch(250)
-        feedbacks = sorted(feedbacks, key=lambda feedback: feedback.sum_votes)[:50]
+        feedbacks = sorted(feedbacks, key=lambda feedback: feedback.sum_votes)
+        feedbacks = feedbacks[:50]
 
-        template_content = {
-                "feedbacks": feedbacks, 
-                "feedback_count": feedback_count,
-                "has_more": len(feedbacks) < feedback_count,
-                "feedback_type_question": discussion_models.FeedbackType.Question,
-                "feedback_type_comment": discussion_models.FeedbackType.Comment,
-                "selected_id": "flaggedfeedback",
+        feedback_type_question = discussion_models.FeedbackType.Question
+        feedback_type_comment = discussion_models.FeedbackType.Comment
+        template_values = {
+                'feedbacks': feedbacks,
+                'feedback_count': feedback_count,
+                'has_more': len(feedbacks) < feedback_count,
+                'feedback_type_question': feedback_type_question,
+                'feedback_type_comment': feedback_type_comment,
+                'selected_id': 'flaggedfeedback',
                 }
 
-        self.render_jinja2_template("discussion/mod/flaggedfeedback.html", template_content)
+        self.render_jinja2_template('discussion/mod/flaggedfeedback.html',
+                                    template_values)
+
 
 class BannedList(request_handler.RequestHandler):
-
     @user_util.moderator_only
     def get(self):
-        banned_user_data_list = user_models.UserData.gql("WHERE discussion_banned = :1", True)
-        self.render_jinja2_template('discussion/mod/bannedlist.html', {
-            "banned_user_data_list" : banned_user_data_list,
-            "selected_id": "bannedlist",
-        })
+        banned_user_data_list = user_models.UserData.gql(
+                'WHERE discussion_banned = :1', True)
+        template_values = {
+            'banned_user_data_list': banned_user_data_list,
+            'selected_id': 'bannedlist',
+        }
+        self.render_jinja2_template('discussion/mod/bannedlist.html',
+                                    template_values)
 
     @user_util.moderator_only
     def post(self):
-        user_data = self.request_user_data("user")
+        user_data = self.request_user_data('user')
 
         if user_data:
-            user_data.discussion_banned = self.request_bool("banned")
+            user_data.discussion_banned = self.request_bool('banned')
             db.put(user_data)
 
             if user_data.discussion_banned:
@@ -96,4 +109,4 @@ class BannedList(request_handler.RequestHandler):
                         feedback.deleted = True
                         feedback.put()
 
-        self.redirect("/discussion/mod/bannedlist")
+        self.redirect('/discussion/mod/bannedlist')
