89fd1203279e50274ce9450d9737bc308ab68ad3
Craig Silverstein <csilvers@khanacademy.org>
2012-04-23 18:22:36 -0700

    Add a test for anointed-consumer.

--- api/v1_endtoend_test.py
+++ api/v1_endtoend_test.py
@@ -137,6 +137,18 @@ class V1EndToEndAuthTest(V1EndToEndTestBase):
         self.set_user(moderator=True)
         self.assert401Error('/api/v1/dev/topictree')
 
+    @testsize.large()
+    def test_anointed_client_access(self):
+        """Only an anointed consumer can access this url."""
+        self.assert401Error('/api/v1/user/videos/SvFtmPhbNRw'
+                            '/log_compatability')
+        self.set_user(anointed_consumer=True)
+        # I don't bother to set all the url-parameters needed, so this
+        # request will fail.  The important point is it gets far
+        # enough into the handling to be looking at the url query fields.
+        self.assert400Error('/api/v1/user/videos/SvFtmPhbNRw'
+                            '/log_compatability')
+
 
 class V1EndToEndGetTest(V1EndToEndTestBase):
     """Test all the GET methods in v1.py, except obsolete /playlist urls."""
