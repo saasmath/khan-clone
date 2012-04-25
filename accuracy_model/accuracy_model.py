f4f371c44782d20663ca9e056920a9e572259018
Craig Silverstein <csilvers@khanacademy.org>
2012-04-25 10:37:18 -0700

    Quiet the linter for our temporary TODO.

--- accuracy_model/accuracy_model.py
+++ accuracy_model/accuracy_model.py
@@ -2,4 +2,4 @@
 # This is here for backwards compatibility with old pickled data.
 # Remove this when we have migrated all data from hard-coding in
 # the old location, to coding in the new one.
-from exercises.accuracy_model import AccuracyModel
+from exercises.accuracy_model import AccuracyModel  # @Nolint
